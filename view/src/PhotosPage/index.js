fde1a488e3fdac29c62743592797b62218507053
TYohoJr <TYohoJr@Gmail.com>
2021-08-08 01:48:06 -0500

    fixed/modified UI to optimize for mobile

--- view/src/PhotosPage/index.js
+++ view/src/PhotosPage/index.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
@@ -424,7 +424,7 @@ class PhotosPage extends Component {
                             <Form.Group controlId="formFile" className="mb-3">
                                 <Form.Label
                                     className="upload-form-label"
-                                >Upload File</Form.Label>
+                                >Upload File:</Form.Label>
                                 <Form.Control
                                     type="file"
                                     accept="image/*"
@@ -432,10 +432,10 @@ class PhotosPage extends Component {
                                 />
                                 <Form.Label
                                     className="upload-form-label upload-form-description-label"
-                                >Short Description</Form.Label>
+                                >Description:</Form.Label>
                                 <Form.Control
                                     type="text"
-                                    placeholder="Short Description"
+                                    placeholder="Optional description of photo"
                                     onChange={(e) => this.handleSelectedFileDescriptionChange(e)}
                                     value={this.state.selectedFileDescription}
                                 />
@@ -447,7 +447,7 @@ class PhotosPage extends Component {
                                     <span className="visually-hidden">Loading...</span>
                                 </Spinner>
                                 :
-                                <span>
+                                <Fragment>
                                     <Button
                                         variant="success"
                                         className="float-right"
@@ -462,7 +462,7 @@ class PhotosPage extends Component {
                                     >
                                         Cancel
                                     </Button>
-                                </span>
+                                </Fragment>
                             }
                         </Modal.Footer>
                     </Modal>
@@ -570,46 +570,50 @@ class PhotosPage extends Component {
                         size="lg"
                         aria-labelledby="contained-modal-title-vcenter"
                         className="view-photo-modal"
+                        backdrop="static"
                         centered
                     >
                         <Modal.Header>
                             <Button // delete
                                 variant="danger"
                                 type="button"
-                                className="float-left"
-                                style={{
-                                    marginRight: '1em',
-                                }}
                                 onClick={() => this.deletePhoto(this.state.selectedPhoto.id)}
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                 </svg>
                             </Button>{' '}
                             <Button // download
-                                variant="success"
+                                variant="primary"
                                 type="button"
-                                className="float-right"
-                                style={{
-                                    marginRight: '1em',
-                                }}
                                 onClick={() => this.downloadPhoto(this.state.selectedPhoto.id)}
                             >
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                                     <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                                     <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                                 </svg>
                             </Button>{' '}
+                            <Button // close
+                                type="button"
+                                variant="secondary"
+                                onClick={() => this.setState({ showPicture: false })}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z" />
+                                </svg>
+                            </Button>
                         </Modal.Header>
                         <Modal.Body>
                             <div className="view-photo-container">
                                 <img className="view-photo-img" alt="" src={this.state.selectedPhotoURL} />
                             </div>
                         </Modal.Body>
-                        <Modal.Footer>
+                        <Modal.Footer
+                            className="photo-details-footer"
+                        >
                             {this.state.isLoading ?
-                                <span>
+                                <Fragment>
                                     <span className="photo-details"><b>FileName:</b> {this.state.selectedPhoto.name}<br /><b>Description:</b> {this.state.selectedPhoto.description}</span>
                                     <Spinner
                                         animation="border"
@@ -618,9 +622,9 @@ class PhotosPage extends Component {
                                     >
                                         <span className="visually-hidden">Loading...</span>
                                     </Spinner>
-                                </span>
+                                </Fragment>
                                 :
-                                <span>
+                                <Fragment>
                                     <span className="photo-details"><b>FileName:</b> {this.state.selectedPhoto.name}<br /><b>Description:</b> {this.state.selectedPhoto.description}</span>
                                     <span className="photo-btns-container">
                                         <Button
@@ -637,13 +641,8 @@ class PhotosPage extends Component {
                                         >
                                             Remove From Album
                                         </Button>{' '}
-                                        <Button
-                                            onClick={() => this.setState({ showPicture: false })}
-                                        >
-                                            Close
-                                        </Button>
                                     </span>
-                                </span>
+                                </Fragment>
                             }
                         </Modal.Footer>
                     </Modal>
