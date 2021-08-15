66f8ea4190cdc256d3753be7236c4d8c850e1140
TYohoJr <TYohoJr@Gmail.com>
2021-08-15 14:44:18 -0500

    testing autoscaling pipeline

--- resources/scripts/cleanup_server.sh
+++ resources/scripts/cleanup_server.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 set -e
 
-cd /app/definitely-not-pb
+# cd /app/definitely-not-pb
 
-# if application is running then stop it
-IS_RUNNING=`docker-compose ps -q app`
-if [[ "$IS_RUNNING" != "" ]]; then
-	docker-compose down
-    docker system prune -a -f
-fi
+# # if application is running then stop it
+# IS_RUNNING=`docker-compose ps -q app`
+# if [[ "$IS_RUNNING" != "" ]]; then
+# 	docker-compose down
+#     docker system prune -a -f
+# fi
 
-# if maintenance page is running then stop it
-IS_RUNNING=`docker ps -q`
-if [[ "$IS_RUNNING" != "" ]]; then
-    docker stop maintenance
-fi
+# # if maintenance page is running then stop it
+# IS_RUNNING=`docker ps -q`
+# if [[ "$IS_RUNNING" != "" ]]; then
+#     docker stop maintenance
+# fi
 
 # copy maintenance page out of project folder
-mkdir -p /app/maintenance
-\cp /app/definitely-not-pb/resources/maintenance/index.html /app/maintenance/index.html
-\cp /app/definitely-not-pb/resources/maintenance/favicon.ico /app/maintenance/favicon.ico
+# mkdir -p /app/maintenance
+# \cp /app/definitely-not-pb/resources/maintenance/index.html /app/maintenance/index.html
+# \cp /app/definitely-not-pb/resources/maintenance/favicon.ico /app/maintenance/favicon.ico
 
 # display maintenance page while app is down
-docker run -it --rm -d -p 80:80 --name maintenance -v /app/maintenance:/usr/share/nginx/html nginx:1.19.6
+# docker run -it --rm -d -p 80:80 --name maintenance -v /app/maintenance:/usr/share/nginx/html nginx:1.19.6
 
-cd /app
+# cd /app
 
-rm -rf definitely-not-pb/
+# rm -rf definitely-not-pb/
--- resources/scripts/start_app.sh
+++ resources/scripts/start_app.sh
@@ -3,14 +3,14 @@ set -e
 
 cd /app/definitely-not-pb
 
-# if maintenance page is running then stop it
-IS_RUNNING=`docker ps -q`
-if [[ "$IS_RUNNING" != "" ]]; then
-    docker stop maintenance
-fi
+# # if maintenance page is running then stop it
+# IS_RUNNING=`docker ps -q`
+# if [[ "$IS_RUNNING" != "" ]]; then
+#     docker stop maintenance
+# fi
 
 docker-compose up -d 
 kill -l
 cd /app
-rm -rf maintenance
+# rm -rf maintenance
 docker ps
